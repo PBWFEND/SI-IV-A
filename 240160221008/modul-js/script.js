a6077d47de893bb8006c624f60b4e265836e18a2
240160221008_dellakartika <240160221008@student.unsap.ac.id>
2026-05-09 15:28:31 +0700

    menyalin index ke modul-css lalu menyalin index css ke modul-js lalu kemudian memperbaiki kodingan index css dan script js

--- 240160221008/modul-js/script.js
+++ 240160221008/modul-js/script.js
@@ -1,24 +1,34 @@
-function generateRandomColor() {
-    var randomColor =
-        '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-    return randomColor;
-}
+const colors = [
+    ["#ffecd2", "#fcb69f"],
+    ["#a1c4fd", "#c2e9fb"],
+    ["#d4fc79", "#96e6a1"],
+    ["#fbc2eb", "#a6c1ee"],
+    ["#84fab0", "#8fd3f4"]
+];
 
 function changeBackgroundColor() {
+
+    const random =
+        colors[Math.floor(Math.random() * colors.length)];
+
     document.body.style.background =
-        "linear-gradient(135deg, " +
-        generateRandomColor() +
-        ", " +
-        generateRandomColor() +
-        ")";
+        `linear-gradient(135deg, ${random[0]}, ${random[1]})`;
 }
 
 function showWelcomeMessage() {
-    alert("Selamat datang di website portofolio Della Kartika!");
+
+    setTimeout(() => {
+
+        alert(
+            "Selamat datang di website portofolio Della Kartika!"
+        );
+
+    }, 500);
 }
 
 window.onload = function () {
+
     changeBackgroundColor();
+
     showWelcomeMessage();
 };
